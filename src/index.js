843011e1fe691756a2af0e578a1090be5b01b30d
--add <kh.magomed2006@mail.ru>
2022-11-16 15:42:30 +0500

    v1.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Users from './component/users';
+import Users from './component/app';
 import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
